147195ec949f84e9553ce633ba6a4d19005dd48b
dev <j32721918@gmail.com>
2026-02-17 20:41:23 -0300

    archivos_resueltos

---
+++ trabajo/src/Screens/Sidebar.jsx
@@ -0,0 +1,8 @@
+import {Contact_card }from  '../assets/Contact_card.jsx';
+function Sidebar(){
+     <div>
+          <div><Contact_card/></div>
+          <div><Contact_card/></div>
+          <div><Contact_card/></div>
+     </div>
+}
